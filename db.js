9c1de04b4ce64a569c438cd5d7c003987ac14760
x-77@live.com <x-77@live.com>
2024-09-06 06:22:34 +0300

    a db function created to reset user progress

--- db.js
+++ db.js
@@ -1,23 +1,50 @@
 const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('sqlite3/dictionary.db', (err) => {
-  if (err) {
-    return console.error(err.message);
+const fs = require('fs');
+async function copyFile(sourcePath, destinationPath) {
+  try {
+    fs.copyFile(sourcePath, destinationPath, (err) => {
+      if (err) {
+        console.error('Error copying file:', err);
+      } else {
+        console.log('File copied successfully.');   
+    
+      }
+    });
+    console.log('File copied successfully.');
+  } catch (err) {
+    console.error('Error:', err);
   }
-  console.log('Connected to the database.');
-  // Query data
-  db.all("SELECT * FROM entries where word = 'minutiae'", (err, rows) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log(rows);
-  });
+}
+const initProgress = () => {
+  // Usage example
+  copyFile('sqlite3/dictionary.db', 'progress/progress.db')
+    .then(() => {
+      setTimeout(() => {
+        const sqlite3 = require('sqlite3').verbose();
+        const db = new sqlite3.Database('progress/progress.db');
+        const col1 = 'ALTER TABLE entries ADD COLUMN created_at DATETIME NULL';
+        const col2 = 'ALTER TABLE entries ADD COLUMN status INTEGER CHECK (status BETWEEN 0 AND 9)';
+        db.run(col1, (err) => {
+          if (err) {
+            console.error('Error adding columns:', err);
+          } else {
+            db.run(col2, (err) => {
+              if (err) {
+                console.error('Error adding columns:', err);
+              } else {
+                console.log("Complete");
+                db.close();
+              }
+            });
+          }
+        });
 
-  // Close the database
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Database closed.');
-  });
-});
+      }, 2000);
+      // Close the database
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+    });
+}
+
+initProgress();
